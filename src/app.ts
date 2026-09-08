6f365e8f6982055c55fa38bc663b2b11f19d78bc
Jeedan <Jeedan.Cuneyt@gmail.com>
2026-09-08 14:18:05 +0300

    added X-Content-Type-Options nosniff header

--- src/app.ts
+++ src/app.ts
@@ -22,79 +22,83 @@ import { createSupportRouter } from "./routes/support.ts";
 import { migrateSensitiveDataAtRest } from "./storage/migrations.ts";
 
 const apiCors: RequestHandler = (req, res, next) => {
-  const origin = req.header("Origin");
+	const origin = req.header("Origin");
 
-  if (origin) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-  }
+	if (origin) {
+		res.setHeader("Access-Control-Allow-Origin", origin);
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+	}
 
-  res.setHeader("Vary", "Origin");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	res.setHeader("Vary", "Origin");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, PATCH, DELETE, OPTIONS",
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Content-Type, Authorization",
+	);
 
-  if (req.method === "OPTIONS") {
-    res.sendStatus(204);
-    return;
-  }
+	if (req.method === "OPTIONS") {
+		res.sendStatus(204);
+		return;
+	}
 
-  next();
+	next();
 };
 
 export function createApp(deps: Dependencies): express.Express {
-  migrateSensitiveDataAtRest(deps.db, deps.keyring);
-  const app = express();
+	migrateSensitiveDataAtRest(deps.db, deps.keyring);
+	const app = express();
 
-  app.use((_req, res, next) => {
-    const cspNonce = randomBytes(16).toString("base64");
-    res.locals.cspNonce = cspNonce;
-    next();
-  });
+	app.use((_req, res, next) => {
+		const cspNonce = randomBytes(16).toString("base64");
+		res.locals.cspNonce = cspNonce;
+		res.set("X-Content-Type-Options", "nosniff");
+		next();
+	});
 
-  app.get("/health", (_req, res) => {
-    res.json({ ok: true, app: "bearly-secure" });
-  });
-  app.use(express.static("public"));
-  app.use(
-    "/vendor/simplewebauthn",
-    express.static("node_modules/@simplewebauthn/browser/dist/bundle"),
-  );
+	app.get("/health", (_req, res) => {
+		res.json({ ok: true, app: "bearly-secure" });
+	});
+	app.use(express.static("public"));
+	app.use(
+		"/vendor/simplewebauthn",
+		express.static("node_modules/@simplewebauthn/browser/dist/bundle"),
+	);
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
-  app.use(createPawPalRouter(deps));
-  app.use(validateRequestOrigin(deps.appOrigin));
-  app.use("/api", apiCors);
-  app.use(createApiRouter(deps));
+	app.use(express.urlencoded({ extended: false }));
+	app.use(express.json());
+	app.use(createPawPalRouter(deps));
+	app.use(validateRequestOrigin(deps.appOrigin));
+	app.use("/api", apiCors);
+	app.use(createApiRouter(deps));
 
-  app.use(createArchiveRouter(deps));
-  app.use(createAssistantRouter(deps));
-  app.use(createImagePreviewRouter(deps));
+	app.use(createArchiveRouter(deps));
+	app.use(createAssistantRouter(deps));
+	app.use(createImagePreviewRouter(deps));
 
-  app.use(createAuthRouter(deps));
-  app.use(createPasskeyRouter(deps));
-  app.use(createAccountRouter(deps));
-  app.use(createCartRouter(deps));
-  app.use(createCheckoutRouter(deps));
-  app.use(createFilesRouter(deps));
-  app.use(createOrdersRouter(deps));
-  app.use(createProductsRouter(deps));
-  app.use(createSupportRouter(deps));
-  app.use(createAdminRouter(deps));
-  app.use(createStorefrontRouter(deps));
+	app.use(createAuthRouter(deps));
+	app.use(createPasskeyRouter(deps));
+	app.use(createAccountRouter(deps));
+	app.use(createCartRouter(deps));
+	app.use(createCheckoutRouter(deps));
+	app.use(createFilesRouter(deps));
+	app.use(createOrdersRouter(deps));
+	app.use(createProductsRouter(deps));
+	app.use(createSupportRouter(deps));
+	app.use(createAdminRouter(deps));
+	app.use(createStorefrontRouter(deps));
 
-  app.use((_req, res) => {
-    sendErrorPage(
-      res,
-      404,
-      "Page Not Found",
-      "We couldn't find the page you requested.",
-    );
-  });
-  app.use(errorHandler);
+	app.use((_req, res) => {
+		sendErrorPage(
+			res,
+			404,
+			"Page Not Found",
+			"We couldn't find the page you requested.",
+		);
+	});
+	app.use(errorHandler);
 
-  return app;
+	return app;
 }
